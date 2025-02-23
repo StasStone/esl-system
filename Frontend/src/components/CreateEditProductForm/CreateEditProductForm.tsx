78063edb53fb3bd6fde22f2b6deb65a3a277f577
StasStone <stanislau.marudau@stud.vilniustech.lt>
2025-02-23 16:42:24 +0200

    feat(CreateEditProductForm): adjust styling

--- Frontend/src/components/CreateEditProductForm/CreateEditProductForm.tsx
+++ Frontend/src/components/CreateEditProductForm/CreateEditProductForm.tsx
@@ -24,76 +24,86 @@ export default function CreateEditProductForm({
 
   return (
     <form className="form" onSubmit={handleSubmit(onSubmit, onError)}>
-      <FormRow label="name" error={errors?.name?.message}>
-        <input
-          className="form-input"
-          type="text"
-          id="name"
-          {...register('name', { required: 'Name is required' })}
-        />
-      </FormRow>
-      <FormRow label="producer" error={errors.producer?.message}>
-        <input
-          className="form-input"
-          type="text"
-          id="producer"
-          {...register('producer', { required: 'Producer is required' })}
-        />
-      </FormRow>
-      <FormRow label="price" error={errors.price?.message}>
-        <input
-          className="form-input"
-          type="text"
-          id="price"
-          {...register('price', {
-            required: 'Price is required',
-            min: { value: 1, message: 'Price should be at least 1' }
-          })}
-        />
-      </FormRow>
-      <FormRow label="sku" error={errors.sku?.message}>
-        <input
-          className="form-input"
-          type="text"
-          id="sku"
-          {...register('sku', {
-            required: 'sku is required'
-          })}
-        />
-      </FormRow>
-      <FormRow label="discount" error={errors.discount?.message}>
-        <input
-          className="form-input"
-          defaultValue={0}
-          id="discount"
-          type="float"
-          {...register('discount', {
-            validate: value =>
-              value! <= getValues().price ||
-              'Discount should be less than regular price'
-          })}
-        />
-      </FormRow>
-      <FormRow label="labels" error={errors.labels?.message}>
-        <input
-          className="form-input"
-          type="text"
-          id="labels"
-          {...register('labels', { required: 'Label id is required' })}
-        />
-      </FormRow>
-      <FormRow label="inventory_count" error={errors.inventory_count?.message}>
-        <input
-          className="form-input"
-          type="number"
-          id="inventory_count"
-          {...register('inventory_count', {
-            validate: value =>
-              value! <= getValues().inventory_count ||
-              'Discount shouldn`t be less than 0'
-          })}
-        />
-      </FormRow>
+      <div className="form-container">
+        <div className="form-column">
+          <FormRow label="name" error={errors?.name?.message}>
+            <input
+              className="form-input"
+              type="text"
+              id="name"
+              {...register('name', { required: 'Name is required' })}
+            />
+          </FormRow>
+          <FormRow label="producer" error={errors.producer?.message}>
+            <input
+              className="form-input"
+              type="text"
+              id="producer"
+              {...register('producer', { required: 'Producer is required' })}
+            />
+          </FormRow>
+          <FormRow label="price" error={errors.price?.message}>
+            <input
+              className="form-input"
+              type="text"
+              id="price"
+              {...register('price', {
+                required: 'Price is required',
+                min: { value: 1, message: 'Price should be at least 1' }
+              })}
+            />
+          </FormRow>
+        </div>
+
+        <div className="form-column">
+          <FormRow label="sku" error={errors.sku?.message}>
+            <input
+              className="form-input"
+              type="text"
+              id="sku"
+              {...register('sku', {
+                required: 'sku is required'
+              })}
+            />
+          </FormRow>
+          <FormRow label="discount" error={errors.discount?.message}>
+            <input
+              className="form-input"
+              defaultValue={0}
+              id="discount"
+              type="float"
+              {...register('discount', {
+                validate: value =>
+                  value! <= getValues().price ||
+                  'Discount should be less than regular price'
+              })}
+            />
+          </FormRow>
+          <FormRow label="labels" error={errors.labels?.message}>
+            <input
+              className="form-input"
+              type="text"
+              id="labels"
+              {...register('labels', { required: 'Label id is required' })}
+            />
+          </FormRow>
+          <FormRow
+            label="inventory_count"
+            error={errors.inventory_count?.message}
+          >
+            <input
+              className="form-input"
+              type="number"
+              id="inventory_count"
+              {...register('inventory_count', {
+                validate: value =>
+                  value! <= getValues().inventory_count ||
+                  'Discount shouldn`t be less than 0'
+              })}
+            />
+          </FormRow>
+        </div>
+      </div>
       <FormRow>
         <button className="standard-btn" type="submit">
           {product ? 'Update Product' : 'Create Product'}
