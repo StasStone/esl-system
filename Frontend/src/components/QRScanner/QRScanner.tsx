6f00078ccf6b6899374862a131837643b42c4b88
StasStone <stanislau.marudau@stud.vilniustech.lt>
2025-04-04 18:59:48 +0300

    feat(QRScanner): implement a mocked QR and add data validation

--- Frontend/src/components/QRScanner/QRScanner.tsx
+++ Frontend/src/components/QRScanner/QRScanner.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import QrReader from 'react-web-qr-reader'
+import { Label } from '../../models/label'
+import './QRScanner.scss'
 
 const QRScanner = () => {
   const delay = 500
@@ -9,28 +11,49 @@ const QRScanner = () => {
     width: 320
   }
 
-  const [result, setResult] = useState('No result')
+  const [result, setResult] = useState<Label | null>(null)
+  const [error, setError] = useState<string>('')
+  const [scanned, setScanned] = useState<boolean>()
 
   const handleScan = (result: any) => {
     if (result) {
-      setResult(result)
+      try {
+        const parsedData = JSON.parse(result.chunks[1].text)
+        setResult(parsedData)
+        setScanned(true)
+      } catch (error: any) {
+        setError(error.message)
+      }
     }
   }
 
   const handleError = (error: any) => {
     console.log(error)
+    setError('Error scanning the qr code')
+    setScanned(true)
   }
 
   return (
-    <>
-      <QrReader
-        delay={delay}
-        style={previewStyle}
-        onError={handleError}
-        onScan={handleScan}
-      />
-      <p>{result}</p>
-    </>
+    <div className="scanner">
+      {scanned && !error ? (
+        result ? (
+          <div className="scanned-data__container">
+            <p>{result.id}</p>
+            <p>{result.product_id}</p>
+            <p>{result.last_updated || 'Newly created'}</p>
+          </div>
+        ) : (
+          <div>No results, Please try again</div>
+        )
+      ) : (
+        <QrReader
+          delay={delay}
+          style={previewStyle}
+          onError={handleError}
+          onScan={handleScan}
+        />
+      )}
+    </div>
   )
 }
 
