2765e98abca8c8b2325181fe9fa6df4849bd778c
StasStone <stanislau.marudau@stud.vilniustech.lt>
2025-04-12 20:30:49 +0300

    feat: add missing dependencies and Table test

---
+++ Frontend/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    '\\.scss$': 'jest-transform-stub'
+  }
+}
